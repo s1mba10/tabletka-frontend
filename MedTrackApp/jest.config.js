8ab3f445f78ec0b16b66f79cd3a98bdcb219fc98
D. <101098236+s1mba10@users.noreply.github.com>
2025-07-05 19:13:52 +0300

    Delete reminders when course removed

--- MedTrackApp/jest.config.js
+++ MedTrackApp/jest.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   preset: 'react-native',
+  moduleNameMapper: {
+    '\\.(png|jpg|jpeg|svg)$': '<rootDir>/__mocks__/fileMock.js',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|@react-navigation)/)',
+  ],
 };
