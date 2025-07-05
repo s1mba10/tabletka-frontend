e21235470def62ca45bca2db77cd2f30f590c204
D. <101098236+s1mba10@users.noreply.github.com>
2025-07-05 19:29:16 +0300

    Refresh reminders after deleting courses

--- MedTrackApp/jest.config.js
+++ MedTrackApp/jest.config.js
@@ -2,8 +2,13 @@ module.exports = {
   preset: 'react-native',
   moduleNameMapper: {
     '\\.(png|jpg|jpeg|svg)$': '<rootDir>/__mocks__/fileMock.js',
+    '^@notifee/react-native$': '<rootDir>/__mocks__/notifeeReactNative.js',
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(react-native|@react-native|@react-navigation)/)',
+    'node_modules/(?!(react-native|@react-native|@react-navigation|react-native-vector-icons|@react-native-community/datetimepicker|react-native-chart-kit)/)',
+  ],
+  setupFiles: [
+    './node_modules/react-native-gesture-handler/jestSetup.js',
+    '<rootDir>/jestSetup.js',
   ],
 };
